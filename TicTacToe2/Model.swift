af8592a0ac24edb80104897a3fc69107f19fd345
Emil Åkerman <emil@emils-air.lan>
2022-12-06 10:17:37 +0100

    first

---
+++ TicTacToe2/Model.swift
@@ -0,0 +1,32 @@
+//
+//  Model.swift
+//  TicTacToe2
+//
+//  Created by Emil Åkerman on 2022-12-01.
+//
+
+import Foundation
+
+class Model {
+    
+    class Grid { /*
+        var square1 : UIImageView!
+        var square2 : UIImageView!
+        var square3 : UIImageView!
+        var square4 : UIImageView!
+        var square5 : UIImageView!
+        var square6 : UIImageView!
+        var square7 : UIImageView!
+        var square8 : UIImageView!
+        var square9 : UIImageView!
+    */} 
+    class Player {
+        let name : String = ""
+    }
+    class PlayerX : Player {
+        let symbol : String = "X"
+    }
+    class PlayerO : Player {
+        let symbol : String = "X"
+    }
+}
